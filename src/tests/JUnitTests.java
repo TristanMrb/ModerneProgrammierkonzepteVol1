d58130afd2f61b130d4020e7702e6f9adaec2e1f
TristanMrb <trisemig@aol.com>
2019-05-23 21:15:34 +0200

    Update JUnit Tests

--- src/tests/JUnitTests.java
+++ src/tests/JUnitTests.java
@@ -2,26 +2,33 @@ package tests;
 
 import oo.binarysearch.OOBinarySearch;
 import oo.linearsearch.index.OOLinearSearchIndex;
+import oo.linearsearch.list.OOLinearSearchList;
 import oo.quicksort.OOQuickSort;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.swing.text.html.HTMLDocument;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class JUnitTests {
 
     int [] arr = {12, 69, 98, 41, 88, 31, 54, 8, 69, 420};
     int [] arr1 = {8, 12, 31, 41, 54, 69, 69, 88, 98, 420};
     int [] arr2 = {12, 8, 31, 41, 54, 69, 69, 88, 98, 420};
-    OOLinearSearchIndex oolinear;
+    OOLinearSearchIndex oolinearindex;
+    OOLinearSearchList oolinearlist;
     OOQuickSort ooquicky;
     OOBinarySearch oobinary;
 
 
     @Before
     public void setup() {
-        oolinear = new OOLinearSearchIndex();
+        oolinearindex = new OOLinearSearchIndex();
+        oolinearlist = new OOLinearSearchList();
         ooquicky = new OOQuickSort();
         oobinary = new OOBinarySearch(arr1);
     }
@@ -29,8 +36,26 @@ public class JUnitTests {
     @Test
     public void OOLinearSearchIndexCheck ()
     {
-        Assert.assertEquals( oolinear.findInt(arr, 69) , 1);
-        Assert.assertEquals( oolinear.findInt(arr, 6) , -1);
+        Assert.assertEquals( oolinearindex.findInt(arr, 69) , 1);
+        Assert.assertEquals( oolinearindex.findInt(arr, 6) , -1);
+    }
+
+    @Test
+    public void OOLinearSearchListCheck ()
+    {
+        boolean expectedResult = false;
+        List<Integer> list = oolinearlist.findInt(arr, 69);
+        Iterator i = list.iterator();
+        while( i.hasNext() ) {
+
+            if ( (int) i.next() == 1) {
+                expectedResult = true;
+            }
+            if ( (int) i.next() == 8 ) {
+                expectedResult = true;
+            }
+        }
+        Assert.assertEquals( expectedResult , true);
     }
 
     @Test
@@ -55,4 +80,5 @@ public class JUnitTests {
         Assert.assertEquals(oobinary.searchNumber(69), 5);
         Assert.assertEquals(oobinary.searchNumber(89), -1);
     }
+
 }
