d311e9b468778887965df92c878fab0d1437a81c
TristanMrb <trisemig@aol.com>
2019-05-23 20:33:45 +0200

    Update JUnit4 Tests

--- src/tests/JUnitTests.java
+++ src/tests/JUnitTests.java
@@ -9,20 +9,25 @@ import org.junit.Test;
 
 public class JUnitTests {
 
+    int [] arr;
+    OOLinearSearchIndex oolinear;
+    OOQuickSort ooquicky;
+    OOBinarySearch oobinary;
+
+
     @Before
     public void setup() {
-        int[] arr = {12, 69, 98, 41, 88, 31, 54, 8, 69, 420};
-        int searchObject = 69;
+        arr = {12, 69, 98, 41, 88, 31, 54, 8, 69, 420};
 
-        OOLinearSearchIndex oolinear = new OOLinearSearchIndex();
-        OOQuickSort ooquicky = new OOQuickSort();
-        OOBinarySearch oobinary = new OOBinarySearch(arr);
+        oolinear = new OOLinearSearchIndex();
+        ooquicky = new OOQuickSort();
+        oobinary = new OOBinarySearch(arr);
     }
 
     @Test
     public void OOLinearSearchIndexCheck ()
     {
-
+        Assert.assertEquals( oolinear.findInt(69, 69) , 1);
     }
 
     @Test
