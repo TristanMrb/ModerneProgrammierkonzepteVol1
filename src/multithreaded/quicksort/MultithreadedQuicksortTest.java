323cf53c598cdf60c19611eec600ac34987e1aaf
NerdyStuff <NerdyStuff@users.noreply.github.com>
2019-05-23 22:57:32 +0200

    Updated documentation

--- src/multithreaded/quicksort/MultithreadedQuicksortTest.java
+++ src/multithreaded/quicksort/MultithreadedQuicksortTest.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MultithreadedQuicksortTest {
+    /**
+     * Test method for multithreaded quicksort algorithm
+     * @param arr Array to be sorted
+     * @param iterations number of iterations
+     * @param threads Threads to be used
+     * @return returns list of min, max and average time the test took.
+     */
     public static List test(int[] arr, int iterations, int threads) {
         long minTime = Long.MAX_VALUE;
         long maxTime = Long.MIN_VALUE;
--- src/multithreaded/quicksort/QuicksortMultithreaded.java
+++ src/multithreaded/quicksort/QuicksortMultithreaded.java
@@ -3,6 +3,11 @@ package multithreaded.quicksort;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class QuicksortMultithreaded {
+    /**
+     * Creates an object of type Sorter and sorts the given array
+     * @param arr int array, passed by reference
+     * @param threads number of max Threads used for sorting
+     */
     public static void quickSort(int[] arr, int threads)
     {
         final AtomicInteger lThreadCounter = new AtomicInteger(1);
